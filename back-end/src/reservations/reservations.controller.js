23585bb68edeb4a8bbce9ab30328a3d656d820fb
Genesee Harmon <geneseelh@gmail.com>
2023-08-22 13:58:01 -0700

    updated controller file

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -19,31 +19,78 @@ async function list(req, res, next) {
   }
 }
 
+function isDate(dateToCheck) {
+  const time = new Date(dateToCheck).getTime();
+  return !isNaN(time);
+}
+
+function isTime(timeToCheck) {
+  // split at colon
+  // put both strings into variables
+  const [hoursStr, minutesStr] = timeToCheck.split(":");
+  // turn each one into a number
+  const hours = Number(hoursStr);
+  const minutes = Number(minutesStr);
+  // check that hours & minutes are actually numbers
+  if (isNaN(hours) || isNaN(minutes)) {
+    return false;
+  }
+  // check if hours <= 24 && > 0
+  if (hours > 24 || hours < 1) {
+    return false;
+  }
+  // check if mins >= 0 && <= 59
+  if (minutes < 1 || minutes > 59) {
+    return false;
+  }
+  return true;
+}
+
 async function create(req, res, next) {
   try {
     if (!req.body.data) {
       return res.status(400).json({
         error: "Missing data in request body",
       });
-    } else {
-      if (!req.body.data.first_name) {
-        return res.status(400).json({
-          error: "Missing first_name",
-        });
-      }
-      if (!req.body.data.last_name) {
-        return res.status(400).json({
-          error: "Missing last_name",
-        });
-      }
-      if (!req.body.data.mobile_number) {
-        return res.status(400).json({
-          error: "Missing mobile_number",
-        });
-      }
+    }
+    if (!req.body.data.first_name) {
+      return res.status(400).json({
+        error: "Missing first_name",
+      });
+    }
+    if (!req.body.data.last_name) {
+      return res.status(400).json({
+        error: "Missing last_name",
+      });
+    }
+    if (!req.body.data.mobile_number) {
+      return res.status(400).json({
+        error: "Missing mobile_number",
+      });
+    }
+    if (
+      !req.body.data.reservation_date ||
+      !isDate(req.body.data.reservation_date)
+    ) {
+      return res.status(400).json({
+        error: "Missing or invalid reservation_date",
+      });
+    }
+    if (
+      !req.body.data.reservation_time ||
+      !isTime(req.body.data.reservation_time)
+    ) {
+      return res.status(400).json({
+        error: "Missing or invalid reservation_time",
+      });
+    }
+    if (!req.body.data.people || !isNaN(req.body.data.people)) {
+      return res.status(400).json({
+        error: "Missing or invalid people",
+      });
     }
     const data = await reservationsService.create(req.body.data);
-    res.status(201).json({
+    return res.status(201).json({
       data: data,
     });
   } catch (error) {
