f6bb36b1d96d8eeea176f2cd5697b32029319c15
Genesee Harmon <geneseelh@gmail.com>
2023-08-29 13:36:02 -0700

    user story 4, fixed migrations/seeds error for tables

--- back-end/src/db/migrations/20210204181028_createTablesTable.js
+++ back-end/src/db/migrations/20210204181028_createTablesTable.js
@@ -1,3 +1,18 @@
-exports.up = async function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("tables", (table) => {
+    table.increments("table_id").primary();
+    table.string("table_name").notNullable();
+    table.boolean("occupied").defaultTo(false);
+    table.integer("capacity").notNullable();
+    table.integer("reservation_id").defaultTo(null);
+    table
+      .foreign("reservation_id")
+      .references("reservation_id")
+      .inTable("reservations");
+    table.timestamps(true, true);
+  });
+};
 
-exports.down = async function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("tables");
+};
