5fddacdc83050af026ecbf547770f7e25f5b1c96
Genesee Harmon <geneseelh@gmail.com>
2023-09-03 17:08:39 -0700

    updated styling of search component & utilized listReservations function from utils/api for cleaner code

--- front-end/src/search/Search.js
+++ front-end/src/search/Search.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
+import { listReservations } from "../utils/api";
 import ReservationsComponent from "../dashboard/ReservationsComponent";
-import ErrorAlert from "../layout/ErrorAlert";
-require("dotenv").config();
+// import ErrorAlert from "../layout/ErrorAlert";
+// require("dotenv").config();
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 function Search() {
-  const [formData, setFormData] = useState({});
-  const [reservations, setReservations] = useState([]);
   const [searched, setSearched] = useState(false);
-  const [error, setError] = useState(null);
+  const [reservations, setReservations] = useState([]);
+  const [formData, setFormData] = useState({});
+  // const [error, setError] = useState(null);
 
   function handleChange({ target }) {
     setFormData({
@@ -22,54 +23,44 @@ function Search() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    async function search() {
-      const abortController = new AbortController();
-      const signal = abortController.signal;
-      try {
-        const response = await axios.get(
-          `${API_BASE_URL}/reservations?mobile_number=${formData.mobile_number}`,
-          { signal }
-        );
-        setReservations(response.data.data);
-        setSearched(!searched);
-      } catch (error) {
-        setError(error.response.data.error);
-        console.log(error, "error loading tables");
-      }
-    }
-    search();
-  }
+    setSearched(true)
+    const abortController = new AbortController();
+    listReservations( formData, abortController.signal )
+    .then((response)=>setReservations(response))
+    .catch((error)=>console.log(error));
+    return () => abortController.abort()
+  };
 
   return (
-    <div className="search">
-      <h1>Search</h1>
-      <form className="form-inline" onSubmit={handleSubmit}>
-        <input
-          className="form-control mr-sm-2"
-          name="mobile_number"
-          type="tel"
-          placeholder="Enter a customer's phone number"
-          aria-label="Search"
-          onChange={handleChange}
-        />
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Find
-        </button>
-      </form>
-      <div className="reservations">
-        {reservations.length
-          ? reservations.map((reservation) => (
-              <ReservationsComponent
-                key={reservation.reservation_id}
-                reservation={reservation}
-              />
-            ))
-          : null}
-        {searched && !reservations.length ? <p>No reservations found</p> : null}
+    <div>
+    <form className="mt-2" name="search_for_number"
+    onSubmit={handleSubmit}
+    >
+    <label>
+      <h2>Search by Mobile Number</h2>
+    </label>
+    <div className="row">
+      <div className="col-lg-8 col-sm-6 col-xs-8">
+      <input onChange={handleChange} 
+      type="search" 
+      name="mobile_number" 
+      className="form-control"
+      placeholder="Enter a customer's phone number" 
+      required />
       </div>
-      <ErrorAlert error={error} />
+      <div className="col-lg-4 col-sm-6 col-xs-4 mt-1">
+    <button type="submit" className="btn btn-primary">
+      Find
+    </button>
     </div>
-  );
+    </div>
+    </form>
+    <div className="mt-4">
+    {reservations.length !== 0 ? <ReservationsComponent reservations={reservations}/> : null}  
+    {searched === true && reservations.length === 0 ? `No reservations found with this phone number` : null}
+    </div>
+    </div>
+  )
 }
 
 export default Search;
